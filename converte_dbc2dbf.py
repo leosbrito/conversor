e0a7e8e6e94fd785a92ffb29dd5b44532cc05a10
leonardobrito.santos <leonardobrito.santos@saude.gov.br>
2024-05-03 14:39:14 -0300

    [update] Alteração do arquivo de exemplo converte_dbc2dbf.py com limpeza de comentários

--- converte_dbc2dbf.py
+++ converte_dbc2dbf.py
@@ -1,12 +1,11 @@
 import conversor_dbc
- 
-# diretorioDBC = "dados/dbc"
 
 def main():
     # Converte arquivos DBC para arquivos DBF no diretórios estipulados
     # Caso não exista diretório ou argumento para eles será criado um diretório chamado "dbf" no raiz do arquivo executado
-    # conversor_dbc.converter_dbc2dbf(diretorioDBC = "dados/dbc", diretorioDBF="dados/dbf")
+    # conversor_dbc.converter_dbc2dbf(arquivoDBF="", diretorioDBC = "dados/dbc", diretorioDBF="dados/dbf")
 
+    # Converte arquivos DBF para arquivos PARQUET no diretórios estipulados ou de um arquivo específico
     conversor_dbc.converter_dbf2parquet(arquivoDBF="dados/dbf/RDMG2209.dbf", diretorioParquet="dados/parquet", row_group=5000)
 
     # conversor_dbc.converter_parquet2csv(ultimo_arquivo_dbf="dados/dbf/RDMG2209.dbf", diretorioParquet="dados/parquet", diretorioCSV="dados/csv")
